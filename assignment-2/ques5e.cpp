b84c522db6d1ac0713d01eca457c50b3a56c7656
MaNaS799 <manasm0609@gmail.com>
2025-09-14 23:21:38 +0530

    Update ques5e.cpp

--- assignment-2/ques5e.cpp
+++ assignment-2/ques5e.cpp
@@ -1,41 +1,47 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main()
-{
-   int n;
-    cout<<"enter the number of elements :\n";
-    cin>>n;
-    int arr[(n*(n+1))/2];
-    cout<<"enter the elements :\n";
-    for(int i=0;i<(n*(n+1))/2;i++)
-    {
-        cin>>arr[i];
-    } 
-    cout<<"2d array representation :\n";
-    int ar[n][n];
-    int s=0;
-    for(int i=0;i<n;i++)
-    {
-        
-     
-        for(int j=0;j<n;j++)
-        {
-        
-            if(j>=i)
- {
-                ar[i][j]=arr[s];
-                cout<<ar[i][j]<<" ";
-                s++;
-            }
-            else
-            {
-                ar[i][j]=ar[j][i];
-                cout<<ar[i][j]<<" ";
-                
 
+int main() {
+    int n = 3;  
+
+    
+    int matrix[3][3] = {
+        {1, 2, 3},
+        {2, 4, 5},
+        {3, 5, 6}
+    };
+
+    
+    int size = n * (n + 1) / 2;
+    int symm[size];
+    int index = 0;
+
+    
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j <= i; j++) {
+            symm[index++] = matrix[i][j];
+        }
+    }
+
+    
+    cout << "The matrix is:" << endl;
+    for(int i = 0; i < n; i++) {
+        index = 0;
+        for(int k = 0; k < i; k++)
+            index += k + 1;  
+        for(int j = 0; j < n; j++) {
+            if(j <= i)
+                cout << symm[index + j] << " ";
+            else {
+                
+                int mirror_index = 0;
+                for(int k = 0; k < j; k++)
+                    mirror_index += k + 1;
+                cout << symm[mirror_index + i] << " ";
             }
         }
-        cout<<endl;
+        cout << endl;
     }
-    return 0 ;
+
+    return 0;
 }
