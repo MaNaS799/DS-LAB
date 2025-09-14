5e879cfd1834d97e2d95aa0c041ef1cdc220ebbb
MaNaS799 <manasm0609@gmail.com>
2025-09-14 23:19:08 +0530

    Update ques5c.cpp

--- assignment-2/ques5c.cpp
+++ assignment-2/ques5c.cpp
@@ -1,43 +1,41 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main()
-{
-   int n;
-    cout<<"enter the number of elements :\n";
-    cin>>n;
-    int arr[(n*(n+1))/2];
-    cout<<"enter the elements :\n";
-    for(int i=0;i<(n*(n+1))/2;i++)
-    {
-        cin>>arr[i];
-    } 
-    cout<<"2d array representation :\n";
-    int ar[n][n];
-    int s=0;
-    for(int i=0;i<n;i++)
-    {
-        
-     
-        for(int j=0;j<n;j++)
-        {
-        
-            
-            if(j<=i)
-            {
-                ar[i][j]=arr[s];
-                cout<<ar[i][j]<<" ";
-                s++;
-            }
-            else
-            {
-                ar[i][j]=0;
-                cout<<ar[i][j]<<" ";
-            
-            }
-            
+
+int main() {
+    int n = 3;  
+
+    
+    int matrix[3][3] = {
+        {1, 0, 0},
+        {4, 5, 0},
+        {7, 8, 9}
+    };
+
+    
+    int size = n * (n + 1) / 2;
+    int lower[size];
+    int index = 0;
+
+    
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j <= i; j++) {
+            lower[index++] = matrix[i][j];
         }
-        
-        cout<<endl;
     }
-    return 0 ;
+
+    
+    cout << "The matrix is:" << endl;
+    index = 0;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            if(j <= i) {
+                cout << lower[index++] << " ";
+            } else {
+                cout << "0 ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
 }
