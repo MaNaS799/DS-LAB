e487ffc3cf5277d1aa41667c98bdb24083b36164
MaNaS799 <manasm0609@gmail.com>
2025-09-14 23:15:39 +0530

    Update ques5a.cpp

--- assignment-2/ques5a.cpp
+++ assignment-2/ques5a.cpp
@@ -1,35 +1,33 @@
-//diagonal
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main()
-{
-    int n;
-    cout<<"enter the number of elements is diagonal : \n";
-    cin>>n;
-    int arr[n];
-    cout<<"enter the diagonal elements :\n";
-    for(int i=0;i<n;i++)
-    {
-        cin>>arr[i];
+
+int main() {
+    int n = 3;  
+
+    
+    int matrix[3][3] = {
+        {5, 0, 0},
+        {0, 8, 0},
+        {0, 0, 3}
+    };
+
+    
+    int diagonal[3];
+    for(int i = 0; i < n; i++) {
+        diagonal[i] = matrix[i][i];
     }
-    cout<<"2d array representation :\n";
-    int ar[n][n];
-    for(int i=0;i<n;i++)
-    {
-        for(int j=0;j<n;j++)
-        {
-            if(i==j)
-            {
-                ar[i][j]=arr[i];
-                cout<<ar[i][j]<<" ";
-            }
+
+    
+    cout << "The matrix is:" << endl;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            if(i == j)
+                cout << diagonal[i] << " ";
             else
-            {
-                ar[i][j]=0;
-                cout<<ar[i][j]<<" ";
-            }
+                cout << "0 ";
         }
-        cout << endl; // To print each row on a new line
+        cout << endl;
     }
-    return 0 ;
+
+    return 0;
 }
